eb31852624c2d6cede83be828f974610bd22273c
Who-car <aysulu2645@gmail.com>
2023-11-27 11:05:25 +0300

    feat: use fluent api for validation

---
+++ backend/CodeBusters/Utils/QuizValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CodeBusters.Models;
+using FluentValidation;
+
+namespace CodeBusters.Utils;
+
+public class QuizValidator : AbstractValidator<Quiz>
+{
+    private QuizValidator()
+    {
+        RuleFor(m => m.Questions.Count)
+            .NotEmpty()
+            .WithMessage("Quiz must contain at least one question");
+    }
+    public static FluentValidation.Results.ValidationResult TryValidate(Quiz quiz)
+    {
+        var validator = new QuizValidator();
+        return validator.Validate(quiz);
+    }
+}
---
+++ backend/CodeBusters/Utils/UserValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using CodeBusters.Models;
+using FluentValidation;
+
+namespace CodeBusters.Utils;
+public class UserValidator : AbstractValidator<User>
+{
+    private UserValidator()
+    {
+        RuleFor(m => m.Name)
+            .NotEmpty()
+            .WithMessage("Field 'Name' is required");
+        RuleFor(m => m.Email)
+            .NotEmpty()
+            .WithMessage("Field 'Email' is required");
+        RuleFor(m => m.Password)
+            .NotEmpty()
+            .WithMessage("Field 'Password' is required");
+        
+        RuleFor(m => m.Email)
+            .EmailAddress()
+            .WithMessage("Invalid email address");
+        RuleFor(m => m.Password)
+            .Must(s => Regex.IsMatch(s, "^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_]).{8,}$"))
+            .WithMessage("password must be at least 8 characters length, " +
+                         "contain at least 1 latin symbol, " + 
+                         "1 number, " + 
+                         "1 #?!@$%^&*-_ symbol");
+    }
+    public static FluentValidation.Results.ValidationResult TryValidate(User user)
+    {
+        var validator = new UserValidator();
+        return validator.Validate(user);
+    }
+}
